b951d2c4c6974c55c32a5b21e208cfc4fc51a26e
spookyeep-bots <spookyeep@gmail.com>
2026-06-16 13:06:09 -0400

    e bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "My name is ruby!",
         // Activity type number (0 = Playing).
-        type: 4, 
+        type: 3, 
       },
     ],
   },
